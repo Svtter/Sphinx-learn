a5b2c6cdfcae1662c0e7be8c2e55856d8801a0c0
svtter <svtter@qq.com>
2018-03-30 19:53:29 +0800

    add github support

--- source/conf.py
+++ source/conf.py
@@ -83,7 +83,7 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'nature'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -196,3 +196,11 @@ intersphinx_mapping = {'https://docs.python.org/': None}
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = True
+
+html_context = {
+    "display_github": True,
+    "github_user": "svtter",
+    "github_repo": "Sphinx-learn",
+    "github_version": "master",
+    "conf_py_path": "/source/"
+}
